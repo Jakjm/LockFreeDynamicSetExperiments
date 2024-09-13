b412ce6ce4752bf788391d3b2995c0dae134371b
jakj3m <jakj3m@gmail.com>
2024-09-13 10:44:38 -0400

    Updating results.

--- expScript.sh
+++ expScript.sh
@@ -5,28 +5,14 @@ do
 	do
 		for t in {1..5}
 		do
-			taskset -c 0-$((numProcs-1)) ./dynamicSetTest -k 20 $s -n $numProcs --output results/predHeavy.csv -O 1 1 8 0 2> errors.txt
+			taskset -c 0-$((numProcs-1)) ./dynamicSetTest -k 20 $s -n $numProcs --output results/insertHeavy.csv -O 8 2 4 4 2> errors.txt
 		done
 	done
 	for numProcs in {1,2,3,4,5,6,8,16,32,64,128,256}
 	do
 		for t in {1..5}
 		do
-			taskset -c 0-$((numProcs-1)) ./dynamicSetTest -k 20 $s -n $numProcs --output results/searchHeavy.csv -O 1 1 0 8 2> errors.txt  
-		done
-	done
-	for numProcs in {1,2,3,4,5,6,8,16,32,64,128,256}
-	do
-		for t in {1..5}
-		do
-			taskset -c 0-$((numProcs-1)) ./dynamicSetTest -k 20 $s -n $numProcs --output results/insertHeavy.csv -O 8 2 2 2 2> errors.txt
-		done
-	done
-	for numProcs in {1,2,3,4,5,6,8,16,32,64,128,256}
-	do
-		for t in {1..5}
-		do
-			taskset -c 0-$((numProcs-1)) ./dynamicSetTest -k 20 $s -n $numProcs --output results/removeHeavy.csv -O 2 8 2 2 2> errors.txt
+			taskset -c 0-$((numProcs-1)) ./dynamicSetTest -k 20 $s -n $numProcs --output results/removeHeavy.csv -O 2 8 4 4 2> errors.txt
 		done
 	done
 done
