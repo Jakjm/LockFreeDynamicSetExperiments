e9216b8412f32ad1aca1e528b895c484573f958d
jakj3m <jakj3m@gmail.com>
2024-09-13 08:11:07 -0400

    Adding more results.

--- expScript.sh
+++ expScript.sh
@@ -1,30 +1,32 @@
 #!/bin/bash
-s=--augTrie
-for numProcs in {1,2,3,4,5,6,8,16,32,64,128,256}
+for s in {--trie,--skip,--augTrie}
 do
-	for t in {1..5}
+	for numProcs in {1,2,3,4,5,6,8,16,32,64,128,256}
 	do
-		taskset -c 0-$((numProcs-1)) ./dynamicSetTest -k 20 $s -n $numProcs --output results/evenDistHT.csv 2> errors.txt
+		for t in {1..5}
+		do
+			taskset -c 0-$((numProcs-1)) ./dynamicSetTest -k 20 $s -n $numProcs --output results/predHeavy.csv -O 1 1 8 0 2> errors.txt
+		done
 	done
-done
-for numProcs in {1,2,3,4,5,6,8,16,32,64,128,256}
-do
-	for t in {1..5}
+	for numProcs in {1,2,3,4,5,6,8,16,32,64,128,256}
 	do
-		taskset -c 0-$((numProcs-1)) ./dynamicSetTest -k 22 $s -n $numProcs --output results/evenBigHT.csv 2> errors.txt  
+		for t in {1..5}
+		do
+			taskset -c 0-$((numProcs-1)) ./dynamicSetTest -k 20 $s -n $numProcs --output results/searchHeavy.csv -O 1 1 0 8 2> errors.txt  
+		done
 	done
-done
-for numProcs in {1,2,3,4,5,6,8,16,32,64,128,256}
-do
-	for t in {1..5}
+	for numProcs in {1,2,3,4,5,6,8,16,32,64,128,256}
 	do
-		taskset -c 0-$((numProcs-1)) ./dynamicSetTest -k 20 $s -n $numProcs --output results/queryHeavy.csv -O 1 1 4 4 2> errors.txt
+		for t in {1..5}
+		do
+			taskset -c 0-$((numProcs-1)) ./dynamicSetTest -k 20 $s -n $numProcs --output results/insertHeavy.csv -O 8 2 2 2 2> errors.txt
+		done
 	done
-done
-for numProcs in {1,2,3,4,5,6,8,16,32,64,128,256}
-do
-	for t in {1..5}
+	for numProcs in {1,2,3,4,5,6,8,16,32,64,128,256}
 	do
-		taskset -c 0-$((numProcs-1)) ./dynamicSetTest -k 20 $s -n $numProcs --output results/updateHeavy.csv -O 4 4 1 1 2> errors.txt
+		for t in {1..5}
+		do
+			taskset -c 0-$((numProcs-1)) ./dynamicSetTest -k 20 $s -n $numProcs --output results/removeHeavy.csv -O 2 8 2 2 2> errors.txt
+		done
 	done
 done
